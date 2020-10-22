c2267a25c27b3c7959c25c6baaf00684f8df960a
Rudy Palacios <rupalacios@paypal.com>
2020-10-22 14:40:22 -0600

    [master] Message

--- 6.giphy_client/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders h1 element', () => {
-  const { getByText } = render(<App />);
-  const h1Element = getByText(/Giphy Client/i);
-  expect(h1Element).toBeInTheDocument();
-});
